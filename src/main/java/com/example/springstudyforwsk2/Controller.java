1047fc6d7b21ce58c27f5ddae796b7dbbea8686a
Алексей Зарубин <alken1t15@gmail.com>
2023-04-20 13:35:58 -0500

    Version 3

--- src/main/java/com/example/springstudyforwsk2/Controller.java
+++ src/main/java/com/example/springstudyforwsk2/Controller.java
@@ -1,18 +1,25 @@
 package com.example.springstudyforwsk2;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Configuration
 @RequestMapping("/auth")
 public class Controller {
     @GetMapping("/login")
+    @PreAuthorize("hasAnyRole()")
     public String login(){
         return "login";
     }
     @GetMapping("/success")
-    public String success(){
+    public String success(Authentication authentication){
+        String name = authentication.getName();
+        System.out.println(name);
         return "success";
     }
     @GetMapping("/error")
